a33df6a87cfd5dd20fa371b171d937c097e62609
Alphee <alphee.grosdidier@gmail.com>
2019-05-01 18:12:29 +0200

    Mise a jour mineure

--- module/exemple_fichier.py
+++ module/exemple_fichier.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 import random
+
 ###############Porte code 1########################
 code_bon_1 = random.randint(10,99)
 code_bon_2 = random.randint(10,99)
