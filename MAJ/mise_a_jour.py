22d721b38fb14f9ef450e05109ecbd393e122e5e
Alphee <alphee.grosdidier@gmail.com>
2019-05-01 18:11:08 +0200

    Ecriture d'un script de mise a jour automatique du jeu

---
+++ MAJ/mise_a_jour.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+import shutil
+import tempfile
+import urllib2
+
+import git
+
+
+class MAJ:
+    def __init__(self):
+        self.URL = '''https://raw.githubusercontent.com/
+unprogrameurfrancaisindependant/
+Livre-dont-tu-est-le-heros-version-python/
+game/MAJ/.information'''.replace('\n', '')
+
+        self.fichier_MAJ = '.information'
+
+        self.clone_URL = '''https://github.com/
+unprogrameurfrancaisindependant/
+Livre-dont-tu-est-le-heros-version-python.git'''.replace('\n', '')
+
+    def createur_variables(self, expression, prefix=''):
+        variable_valeur = expression.split('=')
+        setattr(self, str(prefix + variable_valeur[0].strip()),
+                variable_valeur[1].strip())
+
+    def requete_maj(self):
+        req = urllib2.Request(self.URL)
+        handle = urllib2.urlopen(req)
+        self.page_MAJ = handle.read()
+
+        liste = self.page_MAJ.replace('\n', '').split(';')
+        for item in liste:
+            self.createur_variables(item)
+        self.Tipeurs = eval(self.Tipeurs)
+
+    def fichier_maj(self):
+        with open(self.fichier_MAJ, 'r') as f:
+            fichier = f.read()
+            liste = fichier.replace('\n', '').split(';')
+            for item in liste:
+                self.createur_variables(item, 'file_')
+
+    def comparaison(self):
+        version = self.version.split('.')
+        file_version = self.file_version.split('.')
+        for e in range(len(version)):
+            if version[e] > file_version[e]:
+                self.mise_a_jour_du_jeu_complet()
+            elif version[e] < file_version[e]:
+                continue
+
+        date_de_modification = self.date_de_modification.split('-')
+        file_date_de_modification = self.file_date_de_modification.split('-')
+        for e in range(len(date_de_modification)):
+            if date_de_modification[e] > file_date_de_modification[e]:
+                self.mise_a_jour_tipeurs()
+            elif date_de_modification[e] < file_date_de_modification[e]:
+                continue
+
+    def mise_a_jour_du_jeu_complet(self):
+        Chemin_utilisateur = os.path.expanduser('~')
+        Chemin_execution = os.getcwd()
+        try:
+            path_to_dir_temporaire = tempfile.mkdtemp(dir=Chemin_utilisateur)
+            git.Repo.clone_from(self.clone_URL, path_to_dir_temporaire,
+                                branch='game')
+            for files in os.listdir(path_to_dir_temporaire):
+                if files in ['.git', '.sauvegarde']:
+                    shutil.move((path_to_dir_temporaire + files,
+                                 Chemin_execution + files))
+
+        except:
+            message = """Une érreur s'est produite lors de la mise a
+jour, veuillez réessayer ulterieurement""".replace('\n', '')
+
+        finally:
+            try:
+                shutil.rmtree(path_to_dir_temporaire)
+            except:
+                pass
+
+    def mise_a_jour_tipeurs(self):
+        with open(self.fichier_MAJ, 'w') as f:
+            f.write('version = ' + str(self.file_version) + ';\n')
+            f.write('date_de_modification = ' + str
+                    (self.date_de_modification) + ';\n')
+            f.write('Tipeurs = ' + str(self.Tipeurs))
+
+    def mise_a_jour_automatique(self):
+        self.requete_maj()
+        self.fichier_maj()
+        self.comparaison()
+                                
+
+    def __repr__(self):
+        print self.file_version
+        print self.file_date_de_modification
+        print self.Tipeurs
+        return 'lol'
+
+
+if __name__ == '__main__':
+    MAJ = MAJ()
+    MAJ.requete_maj()
+    MAJ.fichier_maj()
+    MAJ.comparaison()
