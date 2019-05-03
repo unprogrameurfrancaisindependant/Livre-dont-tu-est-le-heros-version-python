ff26bb0acd9f0465748f997029df96056ae26a56
Alphee <alphee.grosdidier@gmail.com>
2019-05-03 18:09:55 +0200

    Reparation

--- Jeu.py
+++ Jeu.py
@@ -1,18 +1,18 @@
-
 #!/usr/bin/env python2.7
-# -*- coding:Utf-8 -*- 
+# -*- coding:Utf-8 -*-
 from MAJ import MAJ
 from module import Game_Master, clear_files, set_permissions
 
+set_permissions('execute')
+
 MAJ().mise_a_jour_automatique()
 
-set_permissions('execute')
 
 Pas_d_idee = Game_Master()
 Pas_d_idee.deroulement_jeu()
 Pas_d_idee.fin_de_la_partie()
 # Fonction qui doit disparaitre
 
-set_permissions('lock')
+#set_permissions('lock')
 
 clear_files()
